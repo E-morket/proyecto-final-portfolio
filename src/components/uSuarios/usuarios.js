8c25e2101e7a3a8578b3ecc6fcdc4df7b51066bb
Esteban Arguedas <arguedas0099@hotmail.com>
2023-05-03 15:20:51 -0600

    nuevo commit

--- src/components/uSuarios/usuarios.js
+++ src/components/uSuarios/usuarios.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react'
 import axios from 'axios';
 import {
     Alert, Button, Grid, TextField, Box, Paper, TableRow, TableHead, TableContainer,
-    TableCell, TableBody, Table,
+    TableCell, TableBody, Table, ButtonGroup,
 } from '@mui/material'
 
 
